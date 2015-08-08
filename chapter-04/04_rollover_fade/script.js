8e16fe6a42e14774f760a686ab7c6e9c98b0d860
sanchits <sanchitsharma1209@gmail.com>
2015-08-08 05:44:52 +0530

    Revert "Revert "chapter""
    This reverts commit 34ed6220a54f097831a522445a8fab0507c38c52.

---
+++ chapter-04/04_rollover_fade/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#fader').hover(function () {
+        $(this).find('img:eq(1)').stop(true, true).fadeIn();
+    }, function () {
+        $(this).find('img:eq(1)').fadeOut();
+    });
+
+});
